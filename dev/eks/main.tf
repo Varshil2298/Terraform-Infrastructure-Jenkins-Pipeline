64c0ed7f3b82a2a46b71d245e7451217ed6673fe
Varshil-Desai-2298 <desaivarshil5@gmail.com>
2024-12-19 21:47:44 +0530

    Updated main.tf

--- dev/eks/main.tf
+++ dev/eks/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "eks" {
-  source = "../module"
+  source = "../module/networking-kubernetes"
 
   env                   = var.env
   cluster-name          = "${local.env}-${local.org}-${var.cluster-name}"
