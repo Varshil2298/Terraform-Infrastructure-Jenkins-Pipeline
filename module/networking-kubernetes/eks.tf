2eff2f79ae7b38abfd80bb5b72c3decbd280237c
Varshil-Desai-2298 <desaivarshil5@gmail.com>
2024-12-28 15:40:00 +0530

    Update eks.tf

--- module/networking-kubernetes/eks.tf
+++ module/networking-kubernetes/eks.tf
@@ -7,7 +7,7 @@ resource "aws_eks_cluster" "eks" {
 
   vpc_config {
     subnet_ids              = [aws_subnet.private-subnet[0].id, aws_subnet.private-subnet[1].id, aws_subnet.private-subnet[2].id]
-    endpoint_private_access = var.endpoint-private-access
+  #  endpoint_private_access = var.endpoint-private-access
     endpoint_public_access  = var.endpoint-public-access
     security_group_ids      = [aws_security_group.eks-cluster-sg.id]
   }
